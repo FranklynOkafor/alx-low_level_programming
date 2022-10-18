da148f5f67f380109b55810c9959f740fbf2799c
Okafor_Franklyn <frankeokafor@gmail.com>
2022-10-18 16:09:37 -0700

    rush

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * _abs - computes the absolute value of an int
+ * @n: the int in quetion
+ *
+ * Return: the abs of the int
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
