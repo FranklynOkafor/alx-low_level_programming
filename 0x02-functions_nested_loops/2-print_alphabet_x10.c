991bd2c2e1303dbb470dd5ca3027f6cbda64b0cf
Okafor_Franklyn <frankeokafor@gmail.com>
2022-10-18 12:23:35 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 time the alphabet in lower case
+ * written by Dongo
+ *
+ * Avoid uing more than two putchars
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <='z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
