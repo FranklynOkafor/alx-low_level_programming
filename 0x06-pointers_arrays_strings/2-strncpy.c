8df0ecd6505ad5f6a0078b63cafd757e30f30ed1
Okafor_Franklyn <frankeokafor@gmail.com>
2022-10-27 11:01:27 -0700

    push

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strncpy - function that copies a string.
+ * @src:source of tge code
+ * @dest: destination of the code
+ * @n: lenght of code
+ * Written by Dongo😌
+ * return: pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
+	return (dest);
+}
